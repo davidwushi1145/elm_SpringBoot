f98e1c60a467030ff80392c8793c187da14afed3
davidlee <david.wushi@foxmail.com>
2023-12-11 20:30:05 +0800

    移除无用代码

--- src/test/java/com/elm/ElmbootApplicationTests.java
+++ src/test/java/com/elm/ElmbootApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Elmboot2ApplicationTests {
+class ElmbootApplicationTests {
 
 	@Test
 	void contextLoads() {
